0f57b3b73eb5a04d3d1c331e4c0d46b6a54da4f1
Swt623 <77751206+Swt623@users.noreply.github.com>
2021-06-07 21:56:49 -0500

    Update humann2_test.sh
    taxonomic profile
    nucleotide database
    protein database

--- scripts/humann2_test.sh
+++ scripts/humann2_test.sh
@@ -13,8 +13,8 @@
 #SBATCH -p genomicsguest
 
 #SBATCH --mem=12G
-#SBATCH --output=/projects/b1042/HartmannLab/weitao/monkeygut/job_files/metaphlan2_%A_%a.out
-#SBATCH --error=/projects/b1042/HartmannLab/weitao/monkeygut/job_files/metaphlan2_%A_%a.err
+#SBATCH --output=/projects/b1042/HartmannLab/weitao/monkeygut/job_files/humann2_%A_%a.out
+#SBATCH --error=/projects/b1042/HartmannLab/weitao/monkeygut/job_files/humann2_%A_%a.err
 
 # purge all modules
 module purge all
@@ -26,7 +26,10 @@ sample="KM-101"
 echo "HUMAnN2 ${sample} started."
 
 cd /projects/b1042/HartmannLab/weitao/monkeygut/
+
+cat kneaddata_out/${sample}_R1_kneaddata_paired_1.fastq.gz kneaddata_out/${sample}_R1_kneaddata_paired_2.fastq.gz > kneaddata_cat/${sample}.fastq.gz
+
 # run HUMAnN2 in Singularity
-singularity exec -B /projects/b1042/ -B /projects/b1057/ /projects/b1057/biobakery_diamondv0822.sif humann2 -i kneaddata_out/${sample}_ -o humann2_out --input-format sam --threads 12
+singularity exec -B /projects/b1042/ -B /projects/b1057/ /projects/b1057/biobakery_diamondv0822.sif humann2 -i kneaddata_cat/${sample}.fastq.gz -o humann2_out --input-format fastq --threads 12 --taxonomic-profile metaphlan2_out/profiled_${sample}.txt --nucleotide-database /projects/b1057/humann2_ref_data/chocophlan --protein-database /projects/b1057/humann2_ref_data/uniref50
 
 echo "Completed HUMAnN2 ${sample}."
